0aaa5adc05b3dfe87365e68cdc7ab24776b6633e
Blake Hung <blake.hung@atgenomix.com>
2024-05-14 03:34:23 +0800

    feat: profile

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,8 @@
     <NuxtLink to="/admin"> admin </NuxtLink>
     <NuxtLink to="/guidelines"> Guidelines </NuxtLink>
     <NuxtLink to="/login"> login </NuxtLink>
+    <NuxtLink to="/register"> register </NuxtLink>
+    <NuxtLink to="/profile"> profile </NuxtLink>
   </div>
 </template>
 
