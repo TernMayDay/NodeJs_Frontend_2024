04ad7d6e4f7353248d59c0b15a2205982c48dbde
ellayang <ellayang@mail.apex.com.tw>
2024-06-16 21:22:03 +0800

    [修改]vercel error 錯誤訊息

--- stores/NotificationStore.js
+++ stores/NotificationStore.js
@@ -7,11 +7,13 @@ export const useNotificationStore = defineStore('notificationStore', () => {
    * 取得所有通知
    */
   const getNotifications = async () => {
+    // eslint-disable-next-line no-console
     console.error('[待處理]取得所有通知 API - 通知 userId 目前寫死')
     const userId = '664b4ab9e08db863ee132793'
     const data = await useHttp.get(`${apiRoom}/user/${userId}`)
     const { notifications: reslt } = data.data
     notifications.value = reslt
+    // eslint-disable-next-line no-console
     console.error('notifications', reslt)
     return reslt
   }
@@ -21,6 +23,7 @@ export const useNotificationStore = defineStore('notificationStore', () => {
    * @param { string } notificationId 通知 id
    */
   const changeNotificationIsRead = async (notificationId) => {
+    // eslint-disable-next-line no-console
     console.error('[待處理]改變單筆通知讀取狀態 API - 通知 notificationId 、 userId 目前寫死')
     const userId = '664b4ab9e08db863ee132793'
     const data = await useHttp.patch(`${apiRoom}/${notificationId}/user/${userId}`)
