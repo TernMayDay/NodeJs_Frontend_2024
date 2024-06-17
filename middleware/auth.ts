d51a47cb7a35f1e1b9f8cb8f951cef66a45070f0
Blake Hung <blake.hung@atgenomix.com>
2024-06-17 21:41:31 +0800

    chore: 新增middleware 驗證

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,4 +1,13 @@
+import { defineNuxtRouteMiddleware, useRuntimeConfig } from 'nuxt/app';
+
 // 攔截 router 變化
-// export default defineNuxtRouteMiddleware((to, from) => {
-//   console.log('auth')
-// })
+export default defineNuxtRouteMiddleware((to, from) => {
+  if (process.client) {
+    const token = localStorage.getItem('authToken');
+    const config = useRuntimeConfig();
+    console.log(token)
+    if (!token) {
+      return navigateTo('/login');
+    }
+  }
+})
